39453eafb7c921790155a094bde0370590d43468
DanielBernal7 <danbernal@csumb.edu>
2024-02-09 20:33:02 -0800

    Added a header and added index values to the Static fields
    Changed getBookCount()
    returns book.size() instead of BOOK_COUNT_
    
    Got rid of unused import

--- src/test/java/ReaderTest.java
+++ src/test/java/ReaderTest.java
@@ -7,7 +7,6 @@
     Date: 02/22/21
  */
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
