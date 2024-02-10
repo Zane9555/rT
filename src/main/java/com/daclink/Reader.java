3d6d11af9318aad1643202a52e776596da0f7ad6
DanielBernal7 <danbernal@csumb.edu>
2024-02-09 17:12:50 -0800

    Imported the com.daclink.Utitlites.Code; Added addBook, removeBook, hasBook, and toString Added getters and setters and equals and hashCode

--- src/main/java/com/daclink/Reader.java
+++ src/main/java/com/daclink/Reader.java
@@ -1,4 +1,6 @@
 package com.daclink;
+import com.daclink.Utilities.Code;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +10,7 @@ public class Reader {
   public static int NAME_;
   public static int PHONE_;
   public static int BOOK_COUNT_;
-  public static int BOOK_START_;
+  public static int BOOK_START_ = 4;
 
   private int cardNumber;
   private String name;
@@ -21,4 +23,109 @@ public class Reader {
     this.phone = phone;
     books = new ArrayList<>();
   }
+
+  public Code addBook(Book book){
+    if(hasBook(book) ){
+      return Code.BOOK_ALREADY_CHECKED_OUT_ERROR;
+    }
+    books.add(book);
+
+    return Code.SUCCESS;
+  }
+
+  public Code removeBook(Book book){
+    if(!hasBook(book)){
+      return Code.READER_DOESNT_HAVE_BOOK_ERROR;
+    }
+    if(hasBook(book)){
+      books.remove(book);
+      return Code.SUCCESS;
+    }
+
+    return Code.READER_COULD_NOT_REMOVE_BOOK_ERROR;
+  }
+
+  public boolean hasBook(Book book){
+    if(books.contains(book) ){
+      return true;
+    }
+
+    return false;
+  }
+
+  public String toString(){
+    return name + "(#" + cardNumber + ") has checked out " + getBooks();
+  }
+
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Reader reader = (Reader) o;
+
+    if (getCardNumber() != reader.getCardNumber()) {
+      return false;
+    }
+    if (getName() != null ? !getName().equals(reader.getName()) : reader.getName() != null) {
+      return false;
+    }
+    return getPhone() != null ? getPhone().equals(reader.getPhone()) : reader.getPhone() == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = getCardNumber();
+    result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+    result = 31 * result + (getPhone() != null ? getPhone().hashCode() : 0);
+    return result;
+  }
+
+  ///This is getters and setters
+
+
+  public int getBookCount() {
+    BOOK_COUNT_ = books.size();
+    return BOOK_COUNT_;
+  }
+
+  public int getCardNumber() {
+    return cardNumber;
+  }
+
+  public void setCardNumber(int cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public List<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(List<Book> books) {
+    this.books = books;
+  }
+  ////End of getters and setters
+
 }
